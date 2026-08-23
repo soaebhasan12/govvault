34807215b6ab47db3bdb01e45369f9fc83f96ea1
unknown <saibuhasan22@gmail.com>
2026-08-23 19:53:36 +0530

    Fix: Removed Heavy PyTorch and Buggy SDKs, switched to raw Gemini REST API

--- backend/knowledge_base/views.py
+++ backend/knowledge_base/views.py
@@ -6,34 +6,7 @@ from rest_framework.response import Response
 from rest_framework import status
 from .models import DocumentChunk
 from groq import Groq
-# from sentence_transformers import SentenceTransformer
-from .serializers import DocumentSerializer
-from .services import process_and_store_pdf
-from dotenv import load_dotenv
 
-# Ye command .env file se API key nikalegi
-load_dotenv()
-
-class DocumentUploadView(APIView):
-    def post(self, request, *args, **kwargs):
-        # 1. React se aane wale data ko Serializer me daalo
-        serializer = DocumentSerializer(data=request.data)
-        
-        # 2. Check karo ki data valid hai ya nahi (e.g., PDF hi hai na?)
-        if serializer.is_valid():
-            # 3. Database me save karo
-            document = serializer.save()
-            
-            # 4. Background me PDF ko process karne bhej do (Chunking & Embedding)
-            # (Asli production me hum isko Celery me bhejte, par abhi prototype ke liye direct call kar rahe hain)
-            process_and_store_pdf(document.id)
-            
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-      
-      
-      
 def get_gemini_embedding(text):
     api_key = os.environ.get("GEMINI_API_KEY")
     url = f"https://generativelanguage.googleapis.com/v1beta/models/text-embedding-004:embedContent?key={api_key}"
